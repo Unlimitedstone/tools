b2409439c0ae7ab6501ab97cfb0d2360b46b354e
Unlimitedstone <201626811222@zjut.edu.cn>
2024-12-11 16:20:42 +0800

    get hot movie message from web

--- py/get_message_from_web.py
+++ py/get_message_from_web.py
@@ -1,124 +1,126 @@
-import os
-
-from numpy.distutils.conv_template import header
-from selenium import webdriver
-from selenium.common import NoSuchElementException
-from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-
+import dataclasses
 import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 
+@dataclasses.dataclass
 class MovieMessage:
-    def __init__(self,name=''):
-        pass
-    @property
-    def name(self):
-        return self.name
-    @property
-    def tags(self):
-        return self.tags
-    @property
-    def score(self):
-        return self.score
-    @property
-    def image_url(self):
-        return self.image_url
-    @property
-    def common(self):
-        return self.common
-    @property
-    def common_author(self):
-        return self.common_author
+    name: str
+    tags: []
+    score: []
+    image_url: []
+    common: str
+    type: str
+
+
+MovieType = 'Movie'
+TVType = 'TV'
 
 def get_movie_message():
     # 配置webdriver,https://developer.chrome.com/docs/chromedriver/get-started?hl=zh-cn
     service = Service('D:\easy_code\chromedriver-win64\chromedriver.exe')
     service.start()
+    options = Options()
+    options.add_argument("--start-maximized")  # 启动时最大化窗口
+    options.add_argument("--disable-blink-features=AutomationControlled")  # 使浏览器不显示自动化控制的信息
+    options.add_argument("--incognito")  # 启动无痕模式
 
-    hot_movie_message = [MovieMessage()]
-    with webdriver.Chrome(service=service) as driver:
+    hot_movie_message = []
+    with webdriver.Chrome(service=service, options=options) as driver:
         web_url = 'https://movie.douban.com/'
         driver.get(web_url)
-        time.sleep(3)
+        time.sleep(5)
         # 图片的滚动条，下面需要用到
-        slide_wrapper = driver.find_elements(By.CSS_SELECTOR,'slide-wrapper')[0]
-        slide_width = 700
-        max_width = int(driver.execute_script("return arguments[0].style.width;", slide_wrapper).strip("px"))
+        print('获取滚动条')
+        slides = driver.find_elements(By.CLASS_NAME, "slider")
+        if len(slides) == 0:
+            print('no slides')
+            return
+        # 热门电影栏目
+        slide = slides[0]
 
-        actions = ActionChains(driver)
-        current_left = 0
-        while abs(current_left) < max_width:
+        print('获取图片位置')
+        images = slide.find_elements(By.CSS_SELECTOR, "div.cover-wp img")
+        print(f'get images len:{len(images)}')
 
-            images = slide_wrapper.find_elements(By.CSS_SELECTOR, "div.cover-wp img")
+        for image in images:
+            # 鼠标悬停在图片上
+            print('鼠标悬停图片上')
+            move_to_image(image, driver)
+            time.sleep(3)
 
-            for image in images:
-                # 鼠标悬停在图片上
-                actions.move_to_element(image).perform()
-                time.sleep(1)
+            # 提取信息
+            hot_movie_message.append(format_movie_message(driver, image.get_attribute('alt'), image.get_attribute('src')))
 
-                try:
-                    pop_up_windows = driver.find_elements(By.CSS_SELECTOR,'div.detail-pop')
-                    # 提取标题
-                    title_element = pop_up_windows.find_element(By.CSS_SELECTOR, "h3 a")
-                    title = title_element.text.strip()
-                    link = title_element.get_attribute("href")
+    return hot_movie_message
 
-                    # 提取评分
-                    score_element = pop_up_windows.find_element(By.CSS_SELECTOR, "p.rank strong")
-                    score = score_element.text.strip()
 
-                    # 提取额外信息
-                    meta_element = pop_up_windows.find_element(By.CSS_SELECTOR, "p.meta")
-                    meta_info = meta_element.text.strip()
+def format_movie_message(driver, movie_name, image_url):
+    try:
+        print('提取标题')
+        pop_up_windows = driver.find_element(By.CSS_SELECTOR, 'div.detail-pop')
+        # 提取标题
+        title_element = pop_up_windows.find_element(By.CSS_SELECTOR, "h3 a")
+        title = title_element.text.strip()
+        if title == '':
+            title = movie_name
 
-                    # 提取评论
-                    try:
-                        comment_element = pop_up_windows.find_element(By.CSS_SELECTOR, "p.comment")
-                        comment = comment_element.text.strip()
-                    except Exception:
-                        comment = "No comment available"
+        # 提取评分
+        print('提取评分')
+        score_element = pop_up_windows.find_element(By.CSS_SELECTOR, "p.rank strong")
+        score = score_element.text.strip()
 
-                except Exception as e:
-                    print(f'could not find pop up windows:{e}')
+        # 提取标签
+        print('提取标签')
+        meta_element = pop_up_windows.find_element(By.CSS_SELECTOR, "p.meta")
+        meta_info = meta_element.text.strip().split('\n')
 
-                hot_movie_message.append({
-                    "title": title,
-                    "link": link,
-                    "score": score,
-                    "meta_info": meta_info,
-                    "comment": comment
-                })
-            current_left -= slide_width
-            driver.execute_script("arguments[0].style.left = arguments[1] + 'px';", slide_wrapper, current_left)
-            time.sleep(2)  # 等待页面加载
-    print(hot_movie_message)
+        # 提取评论
+        try:
+            print('提取评论')
+            comment_element = pop_up_windows.find_element(By.CSS_SELECTOR, "p.comment")
+            comment = comment_element.text.strip()
+        except Exception:
+            comment = "No comment available"
+
+        return MovieMessage(name=title, tags=meta_info, score=score, image_url=image_url, common=comment, type=MovieType)
+    except Exception as e:
+        print(f'could not find pop up windows:{e}')
+
+
+def move_to_image(image, driver):
+    # 只需要绑定鼠标事件，不需要在页面上出现这个图片。。。
+    hover_script = """
+                    const event = new MouseEvent('mouseover', {
+                        bubbles: true,
+                        cancelable: true,
+                        view: window
+                    });
+                    arguments[0].dispatchEvent(event);
+                """
+    driver.execute_script(hover_script, image)
 
 
 def get_message_from_web():
     pass
 
 
-
-
-
 def start():
-    pass
     # 连接数据库
 
     # 获取豆瓣信息
-
+    get_movie_message()
     #
 
 
-
-
 if __name__ == "__main__":
-    get_movie_message()
-    print('done')
-
-
-
+    start_time = time.time()
+    print('start_time:', start_time)
+    start()
+    end_time = time.time()
+    print('end_time:', end_time)
+    print('done, took:', end_time - start_time)
